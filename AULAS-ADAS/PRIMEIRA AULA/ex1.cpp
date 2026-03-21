18e87f6f055d526824c4344778620f2e81232b30
Emilly Ranny <ranny.emilly@discente.ufg.br>
2026-03-21 18:30:01 -0300

    Create ex1.cpp

---
+++ AULAS-ADAS/PRIMEIRA AULA/ex1.cpp
@@ -0,0 +1,32 @@
+//p incluir biblioteca
+#include <bits/stdc++.h>
+namespace std;
+
+int minain ( ){
+    //declarar a variável "k" como inteira
+    int k, hora;
+    float min;
+
+ //funcao de leia
+    cin >> k;
+
+hora = 21;
+
+(k >=0 && k<10){
+    min= k;
+    cout << hora ":" << "0" << min;
+   } else if (k>=10 && k<60){
+    min= k;
+    cout << hora ":" << min;
+   } else if (k>=60 && k<=70){
+    hora;
+    min= k-60;
+    cout << hora ":" << "0" << min;
+   } else {
+    hora;
+    min= k-60;
+    cout << hora ":" << min;
+   }
+
+
+}
