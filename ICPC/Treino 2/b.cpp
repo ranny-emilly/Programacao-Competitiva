59316937ed1719bdd215cd9b33480018373b71ad
Emilly Ranny <ranny.emilly@discente.ufg.br>
2026-03-21 18:49:53 -0300

    Create b.cpp

---
+++ ICPC/Treino 2/b.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define el "\n";
+
+int main(){
+
+    int q;
+
+    cin >> q;
+
+    vector<int>p;
+
+    while(q--){
+        cin >> int x;
+        p.push_back(x);
+    }
+
+    return 0;
+}
