1dcf71457b8dad495e655ed0093fc2882d4987e4
Emilly Ranny <ranny.emilly@discente.ufg.br>
2026-03-25 10:58:19 -0300

    Update b.cpp

--- ICPC/Treino 2/b.cpp
+++ ICPC/Treino 2/b.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
+<<<<<<< HEAD
 #define el "\n";
 
 int main(){
@@ -15,5 +16,33 @@ int main(){
         p.push_back(x);
     }
 
+=======
+
+int main(){
+
+    int q, x = 0, op = 0;
+    vector <int> a;
+
+    cin >> q;
+
+    for(int i = 0; i < q; i++){
+        cin >> op;
+        
+        if (op != 2) {
+        cin >> x;
+        }
+        if (op == 0){
+            a.push_back(x);
+        }else if(op == 1){
+            cout << a[x] << endl;
+        }else{
+            a.pop_back();
+        }
+        op = 0;
+        x = 0;
+    }
+
+
+>>>>>>> 73616f8b149bc05e7fe05244a6c9947a7662f79d
     return 0;
 }
