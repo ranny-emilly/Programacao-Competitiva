3eab6e77c6480f96cff7b244e1833607ccb0cbf6
Emilly Ranny <ranny.emilly@discente.ufg.br>
2026-03-25 13:43:45 -0300

    Update c.cpp

--- ICPC/Treino 2/c.cpp
+++ ICPC/Treino 2/c.cpp
@@ -3,27 +3,21 @@ using namespace std;
 
 int main(){
 
-    int n, m, x;
+    int n, m, x = 0, indice = 0;
 
     cin >> n >> m;
     vector<int>fila(n);
 
     for(int i = 0; i < n; i++){
         cin >> fila[i];
-        if(fila[i] <= m){
-            fila.pop_back();
+
+        if(fila[i] >= x){
+            x = fila[i];
+            indice = i;
         }
-       
-            if(fila[i] > m){
-            fila[i] -= m;
-            fila.push_back(fila[i]);
-            fila.erase(fila.begin());
-        }
-        
+             
     }
-        while(n--){
-            cout <<fila[n];
-        }
+     cout << indice+1 << endl;  
     
     return 0;
 }
