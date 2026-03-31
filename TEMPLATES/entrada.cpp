c08e14f3afafb7774dc32c6a51deced164cccbc7
Emilly Ranny <ranny.emilly@discente.ufg.br>
2026-03-31 11:20:57 -0300

    Create entrada.cpp

---
+++ TEMPLATES/entrada.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false); //(mais rápido)
+    cin.tie(NULL); // (mais rápido)
+
+    int n;
+    cin >> n;
+
+    return 0;
+}
