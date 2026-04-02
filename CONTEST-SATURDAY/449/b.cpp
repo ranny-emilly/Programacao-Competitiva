6539475272acd3fa060712f1d191bccd1721964c
Emilly Ranny <ranny.emilly@discente.ufg.br>
2026-04-02 17:59:45 -0300

    Create b.cpp

---
+++ CONTEST-SATURDAY/449/b.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int qconsultas, hlinhas, colunas, rdado, cdado;
+
+    cin >> h >> c >> q;
+
+    while (q--){
+        cin >> rdado;
+        cin >> cdado;
+    }
+
+    return 0;
+}
