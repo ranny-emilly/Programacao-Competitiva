380fec15eeccd11d87bd2e813afa3973ef6e3e73
Emilly Ranny <ranny.emilly@discente.ufg.br>
2026-04-27 08:56:58 -0300

    Create d.cpp

---
+++ ICPC/Treino 03/d.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define el "\n";
+
+int main(){
+
+    vector<vector<int>> m, n;
+
+    for(int i = 0; i < 9; i++){
+        for(int j = 0; j < 9; j++){
+            cin >> m[i][j];
+        }
+    }
+    for(int i = 0; i < 9; i++){
+        for(int j = 0; j < 9; j++){
+        vector <int> visto;
+        find(visto.begin, visto.end, )
+        
+    }
+
+
+    return 0;
+}
