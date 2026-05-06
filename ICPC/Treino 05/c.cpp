66582eafd0fdf9f91e18015809c8e71e95716028
Emilly Ranny <ranny.emilly@discente.ufg.br>
2026-05-05 21:59:11 -0300

    Update c.cpp

--- ICPC/Treino 05/c.cpp
+++ ICPC/Treino 05/c.cpp
@@ -1,49 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define el "\n";
+#define el "\n"
 #define _ ios_base::sync_with_stdio(0);cin.tie(0);
 
 int main(){
+    _
 
-int n, x, soma = 0, result = 0;
+    int n, x;
+    cin >> n >> x;
 
-cin >> n >> x;
-vector <int> vec;
-
-while(n--){
-    int y;
-    cin >> y;
-    vec.push_back(y);
-
-}
-
-int l = 0, r = vec.size()-1;
-sort(vec.begin(), vec.end());
-
-
-
-while(r > l){
-
-    soma = vec[r];
-    if(soma == x){
-        l++;
-        result++;        
-        cout << soma << vec[l] << el;
-
-    }else if(soma > x){
-        soma-=vec[r];
-        r--;
-    }else if(soma < x){
-        soma+=vec[l];
-        l++;
+    vector<int> vec(n);
+    for(int i = 0; i < n; i++){
+        cin >> vec[i];
     }
 
-}
+    // ❌ NÃO ordenar!
+    // sort(vec.begin(), vec.end());
+
+    int l = 0;           // ponteiro esquerdo (início da janela)
+    int soma = 0;        // soma da janela atual
+    int result = 0;      // quantidade de subarrays válidos
+
+    // 👉 r vai andando sozinho até o final
+    for(int r = 0; r < n; r++){
+
+        // 🔹 adiciona o elemento atual na soma da janela
+        soma += vec[r];
+
+        // 🔹 enquanto a soma passar do alvo, encolhe a janela pela esquerda
+        while(soma > x){
+            soma -= vec[l]; // remove o elemento da esquerda
+            l++;            // move o ponteiro esquerdo
+        }
+
+        // 🔹 se bateu exatamente o valor, encontramos uma subarray válida
+        if(soma == x){
+            result++;
+
+            // 👉 NÃO mexe no r
+            // 👉 NÃO zera soma
+            // 👉 só continua o processo normal
+        }
+    }
+
     cout << result << el;
-
-
-
 
     return 0;
 }
