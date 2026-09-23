dc96949f32679735570be91e915bb4d9e78ab741
Emilly Ranny <ranny.emilly@discente.ufg.br>
2026-09-23 14:52:11 -0300

    Update Missing_Number.cpp

--- CSES/IP/Missing_Number.cpp
+++ CSES/IP/Missing_Number.cpp
@@ -1,31 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 #define el "\n";
 #define _ ios_base::sync_with_stdio(0);cin.tie(0);
+int MAX = 2*10e5+1;
+vector<int>tem(MAX, 0);
 
+ 
 int main(){_
-
-    #ifndef ONLINE_JUDGE
-    freopen("entrada.txt", "r", stdin);
-#endif
+ 
+//     #ifndef ONLINE_JUDGE
+//     freopen("entrada.txt", "r", stdin);
+// #endif
 
     long long n;
     cin >> n;
-    vector<long long>vec(n);
-    vector<bool>tem(n);
-    for(long long i = 1; i <= n; i++){
-        int a;
-        cin >> a;
-        tem[a] = 1;
+    for(long long i = 0; i < n-1; i++){
+    cin >> tem[i];
     }
 
-    for(long long i = 1; i <= tem.size(); i++){
-        if (tem[i] == 0){
-            cout << i << el;
+    sort(tem.begin(), tem.end());
+ 
+ 
+for(long long i = 0; i < tem.size()-1; i++){
+        if (tem[i] != i+1){
+            cout << i+1 << el;
+            return 0;
         }
     }
-
-
+ 
+ 
     return 0;
 }
